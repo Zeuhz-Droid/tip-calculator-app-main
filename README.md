018eb3b0613e2bd9ce31a159cbad64e39618a9d2
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-07-11 01:59:55 +0100

    Built a tip and calculator app with javascript from scratch to finish. From the HTML and CSS, down to the javascript. These codes contains the writing, fixing, debugging and refactoring of HTML, CSS and most of all JavaScript codes.

---
+++ README.md
@@ -0,0 +1,108 @@
+ontend Mentor - Intro section with dropdown navigation solution
+
+This is a solution to the [Intro section with dropdown navigation challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5).
+
+## Table of contents
+
+- [Table of contents](#table-of-contents)
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the relevant dropdown menus on desktop and mobile when interacting with the navigation links
+- View the optimal layout for the content depending on their device's screen size
+- See hover states for all interactive elements on the page
+
+### Screenshot
+
+![Image After coding](images/Built%20Desktop%20preview.png)
+
+### Links
+
+- Solution URL: [Solution codes](https://github.com/Zeuhz-Droid/intro-section-with-dropdown-navigation-main)
+- Live Site URL: [Hosted Github site](https://zeuhz-droid.github.io/intro-section-with-dropdown-navigation-main/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [JavaScript](https://javascript.org/) - For functionality
+
+### What I learned
+
+I learnt how to structure my javascript codes, as i tried 3 or more times in refactoring my code, not sure i still got the best format, but i know how to go about it when faced with a similar or more tasking challenge. STEP-BY-STEP.
+
+```html
+<div>
+  <label for="custom" class="tip--btn btn-custom">
+    <input
+      type="number"
+      min="1"
+      max="100"
+      id="custom"
+      value=""
+      class="tip--custom"
+      name="tip"
+      placeholder="Custom"
+    />
+  </label>
+</div>
+```
+
+```css
+.input::placeholder {
+  padding: 0;
+  font-size: 2rem;
+  color: var(--light-grayish-cyan);
+}
+
+.input:focus {
+  font-size: 2.3rem;
+  outline: none;
+  color: var(--very-dark-cyan);
+}
+```
+
+```js
+btnCustom.addEventListener("input", function (e) {
+  getCustomAmount(billInput, btnCustom, noOfPeople);
+});
+```
+
+### Continued development
+
+Currently focusing on my JavaScript, brushing it up and hoping to take on newer projects and in the nearest future combine it together with React to effect my HTML markups.
+
+### Useful resources
+
+- [MDN](https://developer.mozilla.org/en-US/) - This helped me for researching values of some css property and Javascript media queries. I really liked this pattern and will use it going forward.
+- [w3 schools](https://www.w3schools.com/colors/colors_converter.asp) - This is an amazing color converter. Helped me convert colors from hsl to rgba.
+
+## Author
+
+- Website - [Zeuhz](coming soon...)
+- Frontend Mentor - [@Zeuhz-Droid](https://www.frontendmentor.io/profile/Zeuhz-Droid)
+- Twitter - [@zeuhzDroid](https://www.twitter.com/zeuhzDroid)
+
+## Acknowledgments
+
+I would love to acknowledge GOD for keeping me alive to write the code for this project and also my tutor Jonas Schedtmann for His deep and detailed explanations.
---
+++ app.js
@@ -0,0 +1,150 @@
+"use strict";
+const billInput = document.querySelector(".bill--input");
+
+const radioBtns = document.querySelectorAll('.tip--input[name="tip"]');
+const btnCustom = document.querySelector(".btn-custom");
+const noOfPeople = document.querySelector(".people--input");
+const tipAmount = document.querySelector(".amount--tip");
+const totalAmount = document.querySelector(".amount--total");
+const btnReset = document.querySelector(".btn--reset");
+
+const spanBlink1 = document.querySelector("#bill");
+const spanBlink2 = document.querySelector("#num");
+
+let total,
+  tip,
+  custom,
+  previousTip,
+  previousTotal,
+  customTip,
+  customTotal,
+  totalAmountPerPerson,
+  tipAmountPerPerson,
+  customTipAmountPerPerson,
+  customTotalAmountPerPerson;
+
+let isChecked = false;
+
+const init = function () {
+  tipAmount.textContent = `$0.00`;
+  totalAmount.textContent = `$0.00`;
+  billInput.value = "";
+  noOfPeople.value = "";
+  btnCustom.children[0].value = "";
+  radioBtns.forEach((btn) => {
+    btn.checked = false;
+  });
+};
+
+btnReset.addEventListener("click", init);
+
+// if (noOfPeople.value < 0) {
+//   spanBlink2.classList.remove(".hide");
+//   spanBlink2.classList.add(".show");
+// }
+
+const getTip = function (bill, btns) {
+  btns.forEach((btn) => {
+    if (btn.checked) {
+      tip = (+btn.value * +bill.value) / 100;
+      previousTip = tip;
+    }
+  });
+};
+getTip(billInput, radioBtns);
+
+const getTipPerPerson = function (bill, btns, num) {
+  btns.forEach((btn) => {
+    if (btn.checked) {
+      tipAmountPerPerson = (+bill.value * +btn.value) / 100 / +num.value;
+      return tipAmountPerPerson;
+    }
+  });
+};
+getTipPerPerson(billInput, radioBtns, noOfPeople);
+
+const getTotalPerPerson = function (bill, btns, num) {
+  btns.forEach((btn) => {
+    if (btn.checked) {
+      totalAmountPerPerson =
+        (+bill.value + (+btn.value * +bill.value) / 100) / +num.value;
+      return totalAmountPerPerson;
+    }
+  });
+};
+getTotalPerPerson(billInput, radioBtns, noOfPeople);
+
+billInput.addEventListener("input", function () {
+  isChecked = false;
+  getTipPerPerson(billInput, radioBtns, noOfPeople);
+  getTotalPerPerson(billInput, radioBtns, noOfPeople);
+  if (noOfPeople.value && isChecked) {
+    tipAmount.textContent = `$${tipAmountPerPerson.toFixed(2)}`;
+    totalAmount.textContent = `$${totalAmountPerPerson.toFixed(2)}`;
+  } else if (noOfPeople.value && !isChecked && btnCustom.children[0].value) {
+    getCustomAmount(billInput, btnCustom, noOfPeople);
+    tipAmount.textContent = `$${tipAmountPerPerson.toFixed(2)}`;
+    totalAmount.textContent = `$${totalAmountPerPerson.toFixed(2)}`;
+  } else {
+    tipAmount.textContent = "$0.00";
+    totalAmount.textContent = "$0.00";
+  }
+});
+
+radioBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    isChecked = true;
+    // getCustomAmount(billInput, btnCustom, noOfPeople);
+    getTipPerPerson(billInput, radioBtns, noOfPeople);
+    getTotalPerPerson(billInput, radioBtns, noOfPeople);
+
+    if (billInput.value && noOfPeople.value) {
+      tipAmount.textContent = `$${tipAmountPerPerson.toFixed(2)}`;
+      totalAmount.textContent = `$${totalAmountPerPerson.toFixed(2)}`;
+    } else {
+      tipAmount.textContent = "$0.00";
+      totalAmount.textContent = "$0.00";
+    }
+    // isChecked = false;
+  });
+});
+
+const getCustomAmount = function (bill, btn, num) {
+  customTip = (+btnCustom.children[0].value * +bill.value) / 100;
+  tipAmountPerPerson = (customTip ? customTip : previousTip) / +num.value;
+  totalAmountPerPerson = (+billInput.value + customTip) / +num.value;
+
+  if (btn.children[0].value && billInput.value && noOfPeople.value) {
+    tipAmount.textContent = `$${tipAmountPerPerson.toFixed(2)}`;
+    totalAmount.textContent = `$${totalAmountPerPerson.toFixed(2)}`;
+  } else {
+    tipAmount.textContent = "$0.00";
+    totalAmount.textContent = "$0.00";
+  }
+};
+
+btnCustom.addEventListener("input", function (e) {
+  getCustomAmount(billInput, btnCustom, noOfPeople);
+});
+
+noOfPeople.addEventListener("input", function () {
+  getTipPerPerson(billInput, radioBtns, noOfPeople);
+  getTotalPerPerson(billInput, radioBtns, noOfPeople);
+  //   getCustomAmount(billInput, btnCustom, noOfPeople);
+
+  if (billInput.value && isChecked) {
+    tipAmount.textContent = `$${tipAmountPerPerson.toFixed(2)}`;
+    totalAmount.textContent = `$${totalAmountPerPerson.toFixed(2)}`;
+  } else if (billInput && !isChecked && btnCustom.children[0].value) {
+    getCustomAmount(billInput, btnCustom, noOfPeople);
+    tipAmount.textContent = `$${tipAmountPerPerson.toFixed(2)}`;
+    totalAmount.textContent = `$${totalAmountPerPerson.toFixed(2)}`;
+  } else {
+    tipAmount.textContent = "$0.00";
+    totalAmount.textContent = "$0.00";
+  }
+});
+
+//////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
