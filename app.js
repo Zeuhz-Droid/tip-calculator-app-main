154848d222a29ae1a1112a0b8ee784b54c05cbe5
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-07-14 18:56:48 +0100

    Updated the app.js --fixed a bug

--- app.js
+++ app.js
@@ -118,12 +118,14 @@ const getCustomAmount = function (bill, btn, num) {
 
 btnCustom.addEventListener("input", function (e) {
   isChecked = false;
-  getCustomAmount(billInput, btnCustom, noOfPeople);
+  if (+e.srcElement.value <= 0 && +e.srcElement.value !== "") {
+    getCustomAmount(billInput, btnCustom, noOfPeople);
+  }
 });
 
 noOfPeople.addEventListener("input", function (e) {
   console.log(e.srcElement.value);
-  if (+e.srcElement.value !== 0 && +e.srcElement.value !== "") {
+  if (+e.srcElement.value <= 0 && +e.srcElement.value !== "") {
     labelText.classList.add("hide");
     labelText.classList.remove("show");
     getTipPerPerson(billInput, radioBtns, noOfPeople);
@@ -141,8 +143,7 @@ noOfPeople.addEventListener("input", function (e) {
       tipAmount.textContent = "$0.00";
       totalAmount.textContent = "$0.00";
     }
-  }
-  if (+e.srcElement.value == 0) {
+  } else if (+e.srcElement.value <= 0) {
     labelText.classList.remove("hide");
     labelText.classList.add("show");
   }
